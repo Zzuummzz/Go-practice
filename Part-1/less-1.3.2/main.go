6cc71e23cd7618371acbb3b22d32c6c70efe3745
Zzuummzz <avkomarovip@gmail.com>
2023-04-08 18:46:48 +0300

    Added less-1.3.2

---
+++ Part-1/less-1.3.2/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+func main() {
+	var (
+		number int
+	)
+
+	// Reading parameters from Stdin
+	_, err := fmt.Scanf("%d", &number)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	//Calculating result
+	counter := make(map[int]int)
+	for ; ; number /= 10 {
+		if number/10 == 0 && number%10 == 0 {
+			break
+		}
+		counter[number%10]++
+	}
+
+	//Writing result to Stdout
+	printCounter(counter)
+}
+
+// printCounter печатает карту в формате
+// key1:val1 key2:val2 ... keyN:valN
+func printCounter(counter map[int]int) {
+	digits := make([]int, 0)
+	for d := range counter {
+		digits = append(digits, d)
+	}
+	sort.Ints(digits)
+	for idx, digit := range digits {
+		fmt.Printf("%d:%d", digit, counter[digit])
+		if idx < len(digits)-1 {
+			fmt.Print(" ")
+		}
+	}
+	fmt.Print("\n")
+}
